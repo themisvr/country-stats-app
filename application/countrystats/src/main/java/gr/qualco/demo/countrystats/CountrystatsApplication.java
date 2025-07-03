cfdaf41382ab825efc9f312c1b505f0a5de437f8
themisvr <varveristhemis@gmail.com>
2025-07-03 15:40:04 +0300

    Initialize backend application for country statistics app

---
+++ application/countrystats/src/main/java/gr/qualco/demo/countrystats/CountrystatsApplication.java
@@ -0,0 +1,13 @@
+package gr.qualco.demo.countrystats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrystatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountrystatsApplication.class, args);
+	}
+
+}
---
+++ application/countrystats/src/test/java/gr/qualco/demo/countrystats/CountrystatsApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.qualco.demo.countrystats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrystatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
