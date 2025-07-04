7a2cbf94745b328cc91747065c89141e9bb370e2
themisvr <varveristhemis@gmail.com>
2025-07-04 15:49:17 +0300

    Configure datasource params from .env file

--- application/countrystats/src/main/java/gr/qualco/demo/countrystats/CountrystatsApplication.java
+++ application/countrystats/src/main/java/gr/qualco/demo/countrystats/CountrystatsApplication.java
@@ -1,5 +1,6 @@
 package gr.qualco.demo.countrystats;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CountrystatsApplication {
 
 	public static void main(String[] args) {
+		loadEnvProperties();
 		SpringApplication.run(CountrystatsApplication.class, args);
+	}
+
+	private static void loadEnvProperties() {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
 	}
 
 }
