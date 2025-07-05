790adf06525fb38cbf6cc197b1c0d4f46c5dd9c8
themisvr <varveristhemis@gmail.com>
2025-07-06 00:05:21 +0300

    Configure application with mock db to pass its test

--- application/countrystats/src/test/java/gr/qualco/demo/countrystats/CountrystatsApplicationTests.java
+++ application/countrystats/src/test/java/gr/qualco/demo/countrystats/CountrystatsApplicationTests.java
@@ -3,8 +3,11 @@ package gr.qualco.demo.countrystats;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.yml")
 class CountrystatsApplicationTests {
 
 	@Test
