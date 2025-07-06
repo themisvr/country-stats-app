88563a2297621702d66ffb1218d011c8d8b095f1
themisvr <varveristhemis@gmail.com>
2025-07-06 16:35:06 +0300

    Remove loading .env variables through dotenv, load them through intellij configuration locally

--- application/countrystats/src/main/java/gr/qualco/demo/countrystats/CountrystatsApplication.java
+++ application/countrystats/src/main/java/gr/qualco/demo/countrystats/CountrystatsApplication.java
@@ -1,6 +1,5 @@
 package gr.qualco.demo.countrystats;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -10,16 +9,6 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 public class CountrystatsApplication {
 
 	public static void main(String[] args) {
-		loadEnvProperties();
 		SpringApplication.run(CountrystatsApplication.class, args);
-	}
-
-	private static void loadEnvProperties() {
-		Dotenv dotenv = Dotenv.configure()
-				.directory("../../")
-				.load();;
-		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
 	}
 }
